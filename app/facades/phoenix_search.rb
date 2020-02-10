187f76418337eb9cd73735ee93c3f2c64e6f7c2d
Rachel Lew <rlew07@gmail.com>
2020-02-10 10:01:02 -0700

    Begin to refactor facade to call on service to make API call instead of making API call directly

--- app/facades/phoenix_search.rb
+++ app/facades/phoenix_search.rb
@@ -9,16 +9,21 @@ class PhoenixSearch
   end
 
   def members
-    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=Gryffindor&orderOfThePhoenix=true")
-
-    member_search_data = JSON.parse(response.body, symbolize_names: true)
-
-    member_search_data.map do |member_data|
+    service.members_by_house(house).map do |member_data|
       Member.new(member_data)
     end
+
+    # existing code
+    # conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=Gryffindor&orderOfThePhoenix=true")
+    #
+    # member_search_data = JSON.parse(response.body, symbolize_names: true)
+    #
+    # member_search_data.map do |member_data|
+    #   Member.new(member_data)
+    # end
   end
 end

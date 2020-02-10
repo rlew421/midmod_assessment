bdbe79580a8e00391066913151b32cd710c68e87
Rachel Lew <rlew07@gmail.com>
2020-02-10 10:27:17 -0700

    Refactor service so that getting/parsing through response is in private, reusable method

--- app/services/harrypotter_service.rb
+++ app/services/harrypotter_service.rb
@@ -3,13 +3,20 @@ class HarrypotterService
     # conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
     #   faraday.adapter Faraday.default_adapter
     # end
+    get_json("characters?key=#{ENV['HP_API_KEY']}&house=#{house}&orderOfThePhoenix=true")
 
-    response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=#{house}&orderOfThePhoenix=true")
-
-    member_search_data = JSON.parse(response.body, symbolize_names: true)
+    # existing code
+    # response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=#{house}&orderOfThePhoenix=true")
+    #
+    # member_search_data = JSON.parse(response.body, symbolize_names: true)
   end
 
   private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
   def conn
     conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|

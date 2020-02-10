ef55a5356ff757b3093242ec01524af49410f090
Rachel Lew <rlew07@gmail.com>
2020-02-10 10:07:36 -0700

    Add test for HarrypotterService

---
+++ spec/services/harrypotter_service_spec.rb
@@ -0,0 +1,15 @@
+describe HarrypotterService do
+  context "instance methods" do
+    it "returns member data" do
+      search = subject.members_by_house(house)
+      expect(search).to be_an Array
+      expect(search.count).to eq 21
+      member_data = search[1]
+
+      expect(member_data).to have_key :name
+      expect(member_data).to have_key :role
+      expect(member_data).to have_key :house
+      expect(member_data).to have_key :patronus
+    end
+  end
+end

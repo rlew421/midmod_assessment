d171006210ad81bead27bc890aaae3051581ce49
Rachel Lew <rlew07@gmail.com>
2020-02-10 09:27:29 -0700

    Add test and SearchController

---
+++ spec/features/user_can_search_members_by_house_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+# As a user,
+# When I visit "/"
+# And I Select “Gryffindor” from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members“
+# Then I should be on page “/search”
+# Then I should see a total of the number of members for that house. (21 for Gryffindor)
+# And I should see a list of the 21 members of the Order of the Phoenix for house Gryffindor.
+#
+# And for each of the members I should see:
+# - The name of the member
+# - The members role (if it exists)
+# - The house the member belongs to
+# - The Patronus of the member (if it exists)
+
+feature "user can search for Gryffindor members of the Order of the Phoenix" do
+  scenario "selecting Gryffindor from dropdown menu" do
+    visit '/'
+
+    select "Gryffindor", from: :house
+
+    click_on "Search For Members"
+
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("21")
+    expect(page).to have_css(".member", count: 21)
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".house")
+      expect(page).to have_css(".patronus")
+    end
+  end
+end

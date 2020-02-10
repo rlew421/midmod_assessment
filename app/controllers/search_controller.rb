c885eeae118877cbb2051480b5e7f165da742f20
Rachel Lew <rlew07@gmail.com>
2020-02-10 09:49:52 -0700

    Begin to render locals from SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=Gryffindor&orderOfThePhoenix=true")
-
-    json = JSON.parse(response.body, symbolize_names: true)
+    render locals: {
+      search_results: PhoenixSearch.new(params[:house])
+    }
+    # existing code
+    # conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=Gryffindor&orderOfThePhoenix=true")
+    #
+    # json = JSON.parse(response.body, symbolize_names: true)
   end
 end

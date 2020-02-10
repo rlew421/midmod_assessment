9f9f18da8999ec9431d5bd78e0d77c6d57d88850
Rachel Lew <rlew07@gmail.com>
2020-02-10 09:39:58 -0700

    Add index method that makes API call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,11 @@
 class SearchController < ApplicationController
+  def index
+    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=Gryffindor&orderOfThePhoenix=true")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 end

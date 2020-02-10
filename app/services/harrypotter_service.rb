3a7779385754e0ba0733fe232e33baae59a4dcee
Rachel Lew <rlew07@gmail.com>
2020-02-10 10:23:08 -0700

    Move API call into private conn method

--- app/services/harrypotter_service.rb
+++ app/services/harrypotter_service.rb
@@ -1,11 +1,19 @@
 class HarrypotterService
   def members_by_house(house)
-    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
+    # conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+    #   faraday.adapter Faraday.default_adapter
+    # end
 
     response = conn.get("characters?key=#{ENV['HP_API_KEY']}&house=#{house}&orderOfThePhoenix=true")
 
     member_search_data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
